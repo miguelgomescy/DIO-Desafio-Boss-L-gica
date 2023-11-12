d8b7b7281213a4b72de503a9a6efbab092d15115
Miguel Gomes <miguelgomescy@gmail.com>
2023-11-12 05:26:07 -0300

    Desafio DIO - Lógica de Programação

---
+++ README.md
@@ -0,0 +1,15 @@
+# Projeto: Boss: Lógica de Programação
+
+
+## Resumo
+Este projeto foi desenvolvido por Miguel Gomes durante o bootcamp de lógica de programação promovido pela empresa DIO.
+
+O desafio foi concluído com sucesso. 
+
+Para contato:
+
+Email: miguel.vieira01@fatec.sp.gov.br
+
++55 (11) 96402-5816
+
+LinkedIn: www.linkedin.com/in/miguelgomescy
---
+++ script.js
@@ -0,0 +1,50 @@
+
+let nome = "Miguelgomescy"
+let xp = 200
+let nivel = ""
+
+if(xp<1000){
+    nivel="Ferro"
+    console.log("O herói de nome " + nome + " está no nível de: " + nivel)
+}
+
+
+if((xp===1001) && (xp<=2000)){
+    nivel="Bronze"
+    console.log("O herói de nome " + nome + " está no nível de: " + nivel)
+}
+
+if((xp===2001) && (xp<=5000)){
+    nivel="Prata"
+    console.log("O herói de nome " + nome + " está no nível de: " + nivel)
+}
+
+
+if((xp===6001) && (xp<=7000)){
+    nivel="Ouro"
+    console.log("O herói de nome " + nome + " está no nível de: " + nivel)
+}
+
+
+if((xp===7001) && (xp<=8000)){
+    nivel="Platina"
+    console.log("O herói de nome " + nome + " está no nível de: " + nivel)
+}
+
+
+if((xp===8001) && (xp<=9000)){
+    nivel="Ascendente"
+    console.log("O herói de nome " + nome + " está no nível de: " + nivel)
+}
+
+
+if((xp===9001) && (xp<=10000)){
+    nivel="Imortal"
+    console.log("O herói de nome " + nome + " está no nível de: " + nivel)
+}
+
+if(xp>=10001){
+    nivel="Radiante"
+    console.log("O herói de nome " + nome + " está no nível de: " + nivel)
+
+}
